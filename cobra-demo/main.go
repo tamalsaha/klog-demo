a0d0a6fe816e629ad94134dfe830b847e1ee3c57
Tamal Saha <tamal@appscode.com>
2021-05-25 00:03:49 -0700

    added-all

--- cobra-demo/main.go
+++ cobra-demo/main.go
@@ -4,7 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"gomodules.xyz/kglog"
+	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/klog/v2"
 	"os"
 )
 
@@ -12,32 +15,54 @@ func NewRootCmd() *cobra.Command {
 	var rootCmd = &cobra.Command{
 		Use:   "hugo",
 		Short: "Hugo is a very fast static site generator",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			klogFlags := flag.NewFlagSet("klog", flag.ExitOnError)
+			klog.InitFlags(klogFlags)
+
+			// Sync the glog and klog flags.
+			cmd.Flags().VisitAll(func(f1 *pflag.Flag) {
+				f2 := klogFlags.Lookup(f1.Name)
+				if f2 != nil {
+					value := f1.Value.String()
+					// Ignore error. klog's -log_backtrace_at flag throws error when set to empty string.
+					// Unfortunately, there is no way to tell if a flag was set to empty string or left unset on command line.
+					_ = f2.Value.Set(value)
+				}
+			})
+		},
 		Run: func(cmd *cobra.Command, args []string) {
 			// Do Stuff Here
 		},
 	}
-	rootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
-	kglog.ParseFlags()
+	//pflag.CommandLine.SetNormalizeFunc(cliflag.WordSepNormalizeFunc)
+	//pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	// kglog.ParseFlags()
+	// pflag.Parse()
 
 	rootCmd.AddCommand(NewGetCmd())
 	return rootCmd
 }
 
 func NewGetCmd() *cobra.Command {
+	var name string
 	var cmd = &cobra.Command{
 		Use:   "get",
 		Short: "hugo get",
 		Run: func(cmd *cobra.Command, args []string) {
-			// Do Stuff Here
+			klog.V(3).Info("name = ", name)
 		},
 	}
-	cmd.Flags().String("n", "tamal", "name")
+	cmd.Flags().StringVarP(&name, "name","n", "tamal", "name")
 
 	return cmd
 }
 
 
 func main() {
+	pflag.CommandLine.SetNormalizeFunc(cliflag.WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	// ParseFlags()
+
 	kglog.InitLogs()
 	defer kglog.FlushLogs()
 
@@ -45,4 +70,22 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func ParseFlags() {
+	flag.Parse()
+
+	klogFlags := flag.NewFlagSet("klog", flag.ExitOnError)
+	klog.InitFlags(klogFlags)
+
+	// Sync the glog and klog flags.
+	flag.CommandLine.VisitAll(func(f1 *flag.Flag) {
+		f2 := klogFlags.Lookup(f1.Name)
+		if f2 != nil {
+			value := f1.Value.String()
+			// Ignore error. klog's -log_backtrace_at flag throws error when set to empty string.
+			// Unfortunately, there is no way to tell if a flag was set to empty string or left unset on command line.
+			_ = f2.Value.Set(value)
+		}
+	})
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/pflag v1.0.5
 	gomodules.xyz/kglog v0.0.3-0.20210525043211-5840a764a960
+	k8s.io/component-base v0.21.1
 	k8s.io/klog/v2 v2.8.0
 )
