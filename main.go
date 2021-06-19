fc67bfa7488c429e42dd51ff9b07e67f2ac6f5d4
Tamal Saha <tamal@appscode.com>
2021-06-19 12:52:47 -0700

    added-all

--- main.go
+++ main.go
@@ -19,11 +19,21 @@ func main() {
 	klog.InitFlags(nil)
 	flag.Set("v", "3")
 	flag.Parse()
-	log := klogr.New().WithName("MyName").WithValues("user", "you")
-	log.Info("hello", "val1", 1, "val2", map[string]int{"k": 1})
-	log.V(3).Info("nice to meet you")
-	log.Error(nil, "uh oh", "trouble", true, "reasons", []float64{0.1, 0.11, 3.14})
-	log.Error(myError{"an error occurred"}, "goodbye", "code", -1)
-	log.V(1).Error(myError{"an error occurred_v(1)"}, "goodbye", "code", -1)
+	logger := klogr.New().WithName("MyName")
+	l1 := logger.WithValues("user", "you")
+	l1.Info("hello", "val1", 1, "val2", map[string]int{"k": 1})
+	l1.V(3).Info("nice to meet you")
+
+	l2 := logger.WithValues("user", "they")
+	l2.Info("hello", "val2", 1, "val2", map[string]int{"k": 1})
+	l2.V(3).Info("nice to meet you")
+	l2.Error(nil, "uh oh", "trouble", true, "reasons", []float64{0.1, 0.11, 3.14})
+	l2.Error(myError{"an error occurred"}, "goodbye", "code", -1)
+	l2.V(1).Error(myError{"an error occurred_v(1)"}, "goodbye", "code", -1)
+
+	l1.Error(nil, "uh oh", "trouble", true, "reasons", []float64{0.1, 0.11, 3.14})
+	l1.Error(myError{"an error occurred"}, "goodbye", "code", -1)
+	l1.V(1).Error(myError{"an error occurred_v(1)"}, "goodbye", "code", -1)
+
 	klog.Flush()
 }
